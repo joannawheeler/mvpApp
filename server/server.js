6997468631e2769baa0dd0c935617a8b25936290
joannawheeler <joannachristinewheeler@gmail.com>
2017-03-13 13:23:33 -0700

    server running

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// app.use(express.static(__dirname + '/../client'));
+
+app.use(bodyParser.json());
+
+//make sure the end directory is the name of the db u create in mongo shell
+mongoose.connect('mongodb://localhost/mvpApp');
+
+var db = mongoose.connection;
+
+
+// require('./routes.js')(app, express);
+
+// app.get('/', function() {
+//   console.log('hello')
+// })
+
+
+app.listen(3000);
+console.log('Running on port 3000');
